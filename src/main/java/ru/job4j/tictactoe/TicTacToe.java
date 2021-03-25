beff254b024207e969a982aeba771b684b42f986
Alexander Pimenov <pimalex1978@yandex.ru>
2021-03-25 18:31:57 +0300

    Небольшие правки в комментариях

--- src/main/java/ru/job4j/tictactoe/TicTacToe.java
+++ src/main/java/ru/job4j/tictactoe/TicTacToe.java
@@ -1,5 +1,8 @@
 package ru.job4j.tictactoe;
 
+/**
+ * Точка входа в программу.
+ */
 public class TicTacToe {
     public static void main(String[] args) {
         Starter starter = new Starter();
